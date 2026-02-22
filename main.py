8f274f7a93a46227e95fa611f566b0d63ea7d100
vdeguzmn <deguzman.voo@obmontessori.edu.ph>
2026-02-22 22:55:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def create_account(e):
     # PASSWORD CHECK
     elif len(password) < 10:
         display("Password must be at least 10 characters long.", target="output")
-
+    # Password must contain at least one Number
     elif password.isalpha():
         display("Password must contain at least one number.", target="output")
-
+    # Password must contain at least one Letter
     elif password.isdigit():
         display("Password must contain at least one letter.", target="output")
-
+    # If all requirements are met, the account is successfully created
     else:
+
         display("✅ Account successfully created!", target="output")
