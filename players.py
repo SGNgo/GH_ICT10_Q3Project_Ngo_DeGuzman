97de6c7239d939c90f02b76c808697df6c7e6c62
SGNgo <ngo.sg@obmontessori.edu.ph>
2026-02-22 22:42:03 +0800

    Add files via upload

--- players.py
+++ players.py
@@ -4,12 +4,32 @@ def player_display(e):
     document.getElementById('players').innerHTML = ""
 
     players = [
-        "Seth Ngo", "Vito De Guzman", "Ishan Shrestha",
-        "Tax Rivera", "Tar Canete", "Enzo Villegas",
-        "Koby Baylon", "LeBron James", "Bronny James",
-        "Aura Briguela", "Bini Mikha", "Daniel Padilla",
-        "Kath Bernardo", "Malupiton", "Jacob Cortez", "Pres. BBM"
+        "ASEO, TESSA",
+        "BATAC, ANAKIN",
+        "CALANGLANG, NERIZA",
+        "DE GUZMAN, VITO",
+        "DEE, AARON",
+        "DOLOR, HARVEY",
+        "GALVEZ, SELENA",
+        "GARCES, ADRIANNA",
+        "GROSPE, JILLIAN",
+        "HIZON, EDUARDO",
+        "INTALAN, MARGO",
+        "KO, ATASHA",
+        "LAGMAN, ALIJAH",
+        "LAW, MARCUS",
+        "MACABAGO, SITTIE",
+        "MARTINEZ, EUAN",
+        "MEDINA, KELSEY",
+        "MENDOZA, MICHAELA",
+        "MERGAL, MANUEL",
+        "NGO, SETH",
+        "PADOJINOG, SOFIA",
+        "RIVERA, BENIGNO",
+        "SHRESTHA, ISHAN",
+        "UY, JENNIFER",
+        "YAO, FRANCESCA"
     ]
 
-    for player in players:
-        display(f"• {player}", target="players")
+    for i in range(len(players)):
+        display(f"{i+1}. {players[i]}", target="players")
